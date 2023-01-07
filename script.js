fc3c82892355227ad7963e36934a0d9d0a34137d
Mikhail Novikau <melmenx@gmail.com>
2023-01-07 21:12:02 +0300

    fix

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ document.addEventListener("DOMContentLoaded", function(){
 document.addEventListener("DOMContentLoaded", function(){
   var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
   console.log(scrollbar);
-  document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+  document.querySelector('[href="#modal-nav"]').addEventListener('click',function(){
     document.body.style.overflow = 'hidden';
-    document.querySelector('#openModal').style.marginLeft = scrollbar;
+    document.querySelector('#modal-nav').style.marginLeft = scrollbar;
   });
   document.querySelector('[href="#close"]').addEventListener('click',function(){
     document.body.style.overflow = 'visible';
-    document.querySelector('#openModal').style.marginLeft = '0px';
+    document.querySelector('#modal-nav').style.marginLeft = '0px';
   });
 });
