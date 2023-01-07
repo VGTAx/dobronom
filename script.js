6ed44870a51831593a9827019b7d9a1e925723eb
Mikhail <102552759+VGTAx@users.noreply.github.com>
2023-01-07 21:09:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ document.addEventListener("DOMContentLoaded", function(){
 document.addEventListener("DOMContentLoaded", function(){
   var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
   console.log(scrollbar);
-  document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+  document.querySelector('[href="#openBtn"]').addEventListener('click',function(){
     document.body.style.overflow = 'hidden';
-    document.querySelector('#openModal').style.marginLeft = scrollbar;
+    document.querySelector('#openBtn').style.marginLeft = scrollbar;
   });
-  document.querySelector('[href="#close"]').addEventListener('click',function(){
+  document.querySelector('[href="#closeBtn"]').addEventListener('click',function(){
     document.body.style.overflow = 'visible';
-    document.querySelector('#openModal').style.marginLeft = '0px';
+    document.querySelector('#closeBtn').style.marginLeft = '0px';
   });
 });
